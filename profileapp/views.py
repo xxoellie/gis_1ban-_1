56a2394e58cf7bd755e473ec69425ba6c5f7aa96
xxoellie <elliesohyeon1202@gmail.com>
2021-07-28 10:19:40 +0900

    7/28 class 1 commit 3

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.models import Profile
 from django.shortcuts import render
 
@@ -9,6 +12,10 @@ from django.views.generic import CreateView, UpdateView
 from profileapp.forms import ProfileCreationForm
 
 
+
+
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ProfileCreateView(CreateView) :
     model = Profile
     form_class = ProfileCreationForm
@@ -19,9 +26,13 @@ class ProfileCreateView(CreateView) :
         form.instance.user = self.request.user
         return super().form_valid(form)
 
+@method_decorator(profile_ownership_required,'get')
+@method_decorator(profile_ownership_required,'post')
 class ProfileUpdateView(UpdateView) :
     model = Profile
     form_class = ProfileCreationForm
     context_object_name = 'target_profile'
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+
