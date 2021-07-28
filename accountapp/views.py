bb2cfc3d65f132c1d7f1ef8a4a01e48d653f0d43
xxoellie <elliesohyeon1202@gmail.com>
2021-07-28 10:38:48 +0900

    7/28 class 1 commit 4

--- accountapp/views.py
+++ accountapp/views.py
@@ -39,8 +39,11 @@ def hello_world(request):
 class AccountCreatView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
 
 class AccountDetailView(DetailView) :
@@ -56,8 +59,11 @@ class AccountUpdateView(UpdateView) :
     model = User
     form_class = AccountCreationForm
     context_object_name = "target_user"
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
     # def get(self,request,*args, **kwargs):
     #     if request.user.is_authenticated and self.get_object() == request.user:
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 
 from profileapp.decorators import profile_ownership_required
@@ -26,13 +26,20 @@ class ProfileCreateView(CreateView) :
         form.instance.user = self.request.user
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse('accountapp:detail',kwargs={'pk': self.object.user.pk})
+
 @method_decorator(profile_ownership_required,'get')
 @method_decorator(profile_ownership_required,'post')
 class ProfileUpdateView(UpdateView) :
     model = Profile
     form_class = ProfileCreationForm
     context_object_name = 'target_profile'
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+
 
 
